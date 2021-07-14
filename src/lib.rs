e1037042893dcb46f5fa30ac4860b88870634be1
james <jcotab@gmail.com>
2021-07-13 21:12:58 -0700

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,6 @@
 use std::error::Error;
 use std::fs;
 
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -18,12 +9,50 @@ pub struct Config {
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
 
         Ok(Config { query, filename }) 
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    
+    for line in search (&config.query, &contents) {
+    println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick there";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
